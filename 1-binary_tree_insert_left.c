67d94e8b6a00937ee534f92ea889bd87751c965e
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-05-15 13:55:39 -0300

    0123

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,6 +7,8 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    binary_tree_t *node;
+
     if(!parent)
     {
         return (NULL);
@@ -19,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     if(parent->left)
     {
         node->left = parent->left;
-        parent->left-parent = node;
+        parent->left->parent = node;
     }
     parent->left = node;
     return (node);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,6 +7,8 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+    binary_tree_t *node;
+
     if(!parent)
     {
         return (NULL);
@@ -19,7 +21,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     if(parent->right)
     {
         node->right = parent->right;
-        parent->right-parent = node;
+        parent->right->parent = node;
     }
     parent->right = node;
     return (node);
