4a09b0984881e477576b84a2c076c8a1e349c93b
root <Nicoou>
2023-05-15 11:23:48 -0700

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,13 +1,26 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
 /**
  * 
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return(0);
-        int left_height = binary_tree_height(tree->left);
-        int right_height = binary_tree_height(tree->right);
+	int left_height = 0;
+	int right_height = 0;
 
-        return (left_height - right_height);
+	if (!tree)
+		return(0);
+
+	if (tree->left)
+	{
+		left_height = binary_tree_height(tree->left);
+		left_height++;
+	}
+	if (tree->right)
+	{
+		right_height = binary_tree_height(tree->right);
+		right_height++;
+	}
+	return (left_height - right_height);
 }
