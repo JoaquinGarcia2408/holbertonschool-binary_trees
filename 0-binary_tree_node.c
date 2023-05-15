2d4abf5522a23fade1daddbadb3d9462665bda92
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-05-15 12:37:00 -0300

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,16 +7,17 @@
 **/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *node;
+    binary_tree_t *node;
 
-node = malloc(sizeof(binary_tree_t));
-if (node == NULL)
-    return (NULL);
+    node = malloc(sizeof(binary_tree_t));
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+    node->parent = parent;
+    node->n = value;
+    node->left = NULL;
+    node->right = NULL;
 
-node->parent = parent;
-node->n = value;
-node->left = NULL;
-node->right = NULL;
-
-return (node);
+    return (node);
 }
